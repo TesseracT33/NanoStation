264d1f46481aca26d848dc5bc2e0908988ef4f7f
Christoffer Ekgrim <christoffer.ekgrim98@hotmail.com>
2023-01-23 09:35:21 +0100

    Name arch struct in host.hpp

--- src/common/host.hpp
+++ src/common/host.hpp
@@ -3,16 +3,16 @@
 #include <bit>
 
 #if defined __x86_64__ || defined _M_X64
-struct {
+struct Arch {
     static constexpr bool x64 = 1;
     static constexpr bool arm64 = 0;
-} constexpr arch;
+} arch;
 #define X64 1
 #elif defined __aarch64__ || defined _M_ARM64
-struct {
+struct Arch {
     static constexpr bool x64 = 0;
     static constexpr bool arm64 = 1;
-} constexpr arch;
+} arch;
 #define ARM64 1
 #else
 #error Unsupported architecture; only x86-64 and arm64 are supported.
