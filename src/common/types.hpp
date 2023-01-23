0a4eb5df3c835be790b18c799604e93b1d7089a5
TesseracT33 <christoffer.ekgrim98@hotmail.com>
2023-01-23 12:57:25 +0100

    Add m128i accessor to Reg128

--- src/common/types.hpp
+++ src/common/types.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "host.hpp"
+
+#include <bit>
 #include <concepts>
 #include <cstddef>
 #include <cstdint>
@@ -15,6 +18,11 @@ using s64 = int64_t;
 
 using uint = unsigned;
 
+#if X64
+#include <immintrin.h>
+using m128i = __m128i;
+#endif
+
 struct Reg128 {
     ::s8 s8() const { return ::s8(lo); }
     ::u8 u8() const { return ::u8(lo); }
@@ -24,7 +32,9 @@ struct Reg128 {
     ::u32 u32() const { return ::u32(lo); }
     ::s64 s64() const { return ::s64(lo); }
     ::u64 u64() const { return ::u64(lo); }
-    void set(std::integral auto data) { lo = data; }
+    ::m128i m128i() const { return std::bit_cast<::m128i>(*this); }
+    void set(std::integral auto val) { lo = val; }
+    void set(::m128i val) { *this = std::bit_cast<Reg128>(val); }
 
 private:
     ::u64 lo, hi;
