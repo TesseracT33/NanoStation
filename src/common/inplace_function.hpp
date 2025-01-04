27ddf19efa4ff3ecf78289bb967319bd39ebac29
TesseracT33 <christoffer.ekgrim98@hotmail.com>
2025-01-04 09:02:43 +0100

    Fix InplaceFunction

--- src/common/inplace_function.hpp
+++ src/common/inplace_function.hpp
@@ -1,30 +1,32 @@
 #pragma once
 
-#include "numtypes.hpp"
 #include <concepts>
+#include <new>
+#include <type_traits>
+
+#include "numtypes.hpp"
 
 template<typename S> class InplaceFunction;
 
 template<typename R, typename... P> class InplaceFunction<R(P...)> {
     static constexpr size_t kCapacity = 16;
 
-public:
-    template<typename F>
-    InplaceFunction(F&& f)
-        requires(sizeof(F) <= kCapacity) && (std::invocable<F, P...>)
-      : invoke_{ invoke<F> }
-    {
-        ::new (static_cast<void*>(storage_)) F(f);
-    }
-
-    R operator()(P... params) const { return invoke_(storage_, params...); }
-
-private:
     template<typename F> static R invoke(void const* storage, P... params)
     {
         return (*static_cast<F const*>(storage))(params...);
     }
 
+    alignas(16) u8 storage_[kCapacity];
     R (*invoke_)(void const*, P...);
-    alignas(8) u8 storage_[kCapacity];
+
+public:
+    template<typename F>
+    InplaceFunction(F&& f)
+        requires(sizeof(F) <= kCapacity && std::invocable<F, P...>)
+      : invoke_{ invoke<std::remove_reference_t<F>> }
+    {
+        ::new (static_cast<void*>(storage_)) std::remove_reference_t<F>(f);
+    }
+
+    R operator()(P... params) const { return invoke_(storage_, params...); }
 };
